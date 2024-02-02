6610d378168a5d3078ca0979b511e7298652b219
Doidae <andyarredondo1224@gmail.com>
2024-02-02 13:57:19 -0500

    Added stubs for other views

---
+++ views/places/edit.jsx
@@ -0,0 +1,6 @@
+const React = require('react')
+const Def = require('../default')
+
+function edit () {}
+
+module.exports = edit
---
+++ views/places/new.jsx
@@ -0,0 +1,8 @@
+const React = require('react')
+const Def = require('../default')
+
+function newPage () {}
+
+module.exports = newPage
+
+// part 3 bonus / check name of function
